da893f1154cdca17ede1bfb00c872a91e242ea1d
ibrahimcbbr@gmail.com <ibrahimcbbr@gmail.com>
2023-01-09 19:45:24 +0300

    Add project files.

---
+++ Infrastructure/CbrBlog.Infrastructure/EfContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CbrBlog.Domain.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CbrBlog.Infrastructure.EfContext
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ Infrastructure/CbrBlog.Infrastructure/Registrations/InfrastructureServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using CbrBlog.Infrastructure.EfContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CbrBlog.Infrastructure.Registrations
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(
+                   configuration.GetConnectionString("BlogConn")));
+
+
+
+            return services;
+        }
+    }
+}
